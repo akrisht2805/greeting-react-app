abb3a490fe5c1beebafd41ec55d1191eee2d1cf1
AKRISHT <akrisht2805@gmail.com>
2021-12-07 13:46:11 +0530

    Done by Using React Component Properties

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from './App'
 
-let curDate = new Date();
-curDate =curDate.getHours();
-let greeting="";
-const cssInline = {};
-
-if(curDate >= 1 && curDate < 12){
-  greeting = 'Good Morning';
-  cssInline.color ='green';
-}else if(curDate >=12 && curDate <19){
-  greeting ='Good Afternoon';
-  cssInline.color ='orange';
-}else{
-  greeting="Good Night";
-  cssInline.color ='black';
-}
-
-
-ReactDOM.render(
-  <>
-  <div>
-  <h1> Hello sir,<span style={cssInline}> {greeting} </span></h1> 
-  </div>
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App/>,document.getElementById("root"));
